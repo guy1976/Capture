dd0b2db8632d63187d4c8534f98f0db6472e6043
guy1976 <guy1976@gmail.com>
2014-04-23 17:28:07 +0300

    asdsad

--- Capture/ViewModel.cs
+++ Capture/ViewModel.cs
@@ -1,33 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Capture
 {
     class ViewModel : DependencyObject
     {
+        DispatcherTimer m_timer;
+        DateTime m_recordingStartTime;
+
         public ViewModel()
         {
             StartRecorderingCommand = new ExtendedCommand(StartRecord, true);
+            StopRecordingCommand = new ExtendedCommand(StopRecord, false);
         }
         public ExtendedCommand StartRecorderingCommand { get; set; }
+        public ExtendedCommand StopRecordingCommand { get; set; }
 
         public void StartRecord()
         {
             StartRecorderingCommand.CanExecuteCommand = false;
+            StopRecordingCommand.CanExecuteCommand = true;
+            IsRecording = true;
+            m_recordingStartTime = DateTime.UtcNow;
+            m_timer = new DispatcherTimer();
+            m_timer.Interval = TimeSpan.FromSeconds(1);
+            m_timer.Tick += m_timer_Tick;
+            m_timer.Start();
+        }
+
+        void m_timer_Tick(object sender, EventArgs e)
+        {
+            RecordingTime = DateTime.UtcNow - m_recordingStartTime;
         }
         public void StopRecord()
         {
             StartRecorderingCommand.CanExecuteCommand = true;
+            StopRecordingCommand.CanExecuteCommand = false;
+            IsRecording = false;
+            m_timer.Stop();
+            m_timer = null;
+        }
+
+
+
+        public bool IsRecording
+        {
+            get { return (bool)GetValue(IsRecordingProperty); }
+            set { SetValue(IsRecordingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsRecording.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsRecordingProperty =
+            DependencyProperty.Register("IsRecording", typeof(bool), typeof(ViewModel), new PropertyMetadata(false));
+
+
+
+
+        public TimeSpan RecordingTime
+        {
+            get { return (TimeSpan)GetValue(RecordingTimeProperty); }
+            set { SetValue(RecordingTimeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RecordingTime.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RecordingTimeProperty =
+            DependencyProperty.Register("RecordingTime", typeof(TimeSpan), typeof(ViewModel), new PropertyMetadata(TimeSpan.Zero));
+
+        
+        
+    }
+    public class BooleanConverter<T> : IValueConverter
+    {
+        public BooleanConverter(T trueValue, T falseValue)
+        {
+            True = trueValue;
+            False = falseValue;
+        }
+
+        public T True { get; set; }
+        public T False { get; set; }
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool && ((bool)value) ? True : False;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is T && EqualityComparer<T>.Default.Equals((T)value, True);
         }
     }
-
-
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter() :
+            base(Visibility.Visible, Visibility.Collapsed) { }
+    }
+    public sealed class BooleanConverter : BooleanConverter<bool>
+    {
+        public BooleanConverter() :
+            base(true, false)
+        {
+        }
+    }
     public class ExtendedCommand : ICommand, INotifyPropertyChanged
     {
         private Action m_executer;
